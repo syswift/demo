f5e7267af280191e0ba81aa09aa7507de3fa0c26
syswift <myworlddai97@gmail.com>
2020-11-08 01:29:36 +1300

    demo

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            
+          )
+        ],
+      ),
+    );
+  }
+}
